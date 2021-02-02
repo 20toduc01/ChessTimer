b747c70d800dcc93084a05318dced58048043810
20toduc01 <20toduc01@gmail.com>
2021-02-03 00:06:55 +0700

    More layout changes

--- main.py
+++ main.py
@@ -10,51 +10,41 @@ def getInput():
         entry.delete(0, tk.END)
         entry.insert(0, text)
 
-    def setTimeLimit():
+    def submitInput():
         try:
             global timeLimit
+            global increment
             timeLimit = int(timeInput.get())
+            increment = int(incInput.get())
+            window.destroy()
         except:
             setEntryText(timeInput, timeLimit)
-    
-    def setIncrement():
-        try:
-            global increment
-            increment = int(incInput.get())
-        except:
             setEntryText(incInput, increment)
-
-    def destroyWindow():
-        window.destroy()
 
 
     window = tk.Tk()
     window.title('Setup')
+    window.resizable(0, 0)
     window.columnconfigure([0, 1], weight = 1, minsize = 100)
-    window.columnconfigure(2, weight = 1, minsize = 50)
     window.rowconfigure([0, 1, 2], weight = 1, minsize = 50)
     
-    timeLabel = tk.Label(master = window, text = 'Time limit: ')
-    timeLabel.grid(row = 0, column = 0, sticky = 'w')
-    timeInput = tk.Entry(master = window, text = '10')
+    timeLabel = tk.Label(master = window, text = 'Time limit')
+    timeLabel.grid(row = 0, column = 0, sticky = 'news')
+    timeInput = tk.Entry(master = window, width=5)
     setEntryText(timeInput, timeLimit)
     timeInput.grid(row = 0, column = 1, sticky = 'w')
-    timeSubmit = tk.Button(master = window, text = 'Set', command = setTimeLimit)
-    timeSubmit.grid(row = 0, column = 2)
 
-    incLabel = tk.Label(master = window, text = 'Increment: ')
-    incLabel.grid(row = 1, column = 0, sticky = 'w')
-    incInput = tk.Entry(master = window, text = '0')
+    incLabel = tk.Label(master = window, text = 'Increment')
+    incLabel.grid(row = 1, column = 0, sticky = 'news')
+    incInput = tk.Entry(master = window, width=5)
     setEntryText(incInput, increment)
     incInput.grid(row = 1, column = 1, sticky = 'w')
-    incSubmit = tk.Button(master = window, text = 'Set', command = setIncrement)
-    incSubmit.grid(row = 1, column = 2)
     
-    submit = tk.Button(master = window, text = 'OK', command = destroyWindow)
+    submit = tk.Button(master = window, text = 'OK', command = submitInput)
     submit.grid(row = 2, column = 0, sticky = 'ew')
 
-    submit = tk.Button(master = window, text = 'Exit', command = exit, width = 11)
-    submit.grid(row = 2, column = 1,)
+    submit = tk.Button(master = window, text = 'Exit', command = exit)
+    submit.grid(row = 2, column = 1, sticky = 'ew')
     window.mainloop()
 
 def run():
@@ -64,7 +54,7 @@ def run():
         second = (clock//10)%60
         milisec = clock%10
         if second < 10: second = '0' + str(second)
-        if clock < 20:
+        if clock < 200:
             return f'{minute}:{second}:{milisec}'
         else:
             return f'{minute}:{second}'
